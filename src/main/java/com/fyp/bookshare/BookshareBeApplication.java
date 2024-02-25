678414e95d2bbbe2bcc8af9bf9e4529f95a5d04b
wenxuan <wen200202@hotmail.com>
2024-02-26 00:55:31 +0800

    Initial commit

---
+++ src/main/java/com/fyp/bookshare/BookshareBeApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshareBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshareBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fyp/bookshare/BookshareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.bookshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshareBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
