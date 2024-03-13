62d787a0462574bff883aafc39b8726162a87ea5
wenxuan <wen200202@hotmail.com>
2024-03-13 20:36:35 +0800

    Mybatis plus pagination config

---
+++ src/main/java/com/fyp/bookshare/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.fyp.bookshare.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author o0wen0o
+ * @create 2024-03-12 10:36 PM
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
